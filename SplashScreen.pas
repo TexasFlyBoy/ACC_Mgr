228581bf23f1bc82905d2be284335af8ee207687
TexasFlyBoy <n2o4nto@gmail.com>
2018-10-03 14:47:34 -0500

    Splash Screen works.
    Had to abandon the FireMonkey method. The other method I had originally tried, but didn't work, did finally work.
       modified:   SSCA_ACDR.DPR
       modified:   SSCA_ACDR.dproj
       new file:   SplashScreen.dfm
       new file:   SplashScreen.pas

---
+++ SplashScreen.pas
@@ -0,0 +1,45 @@
+unit SplashScreen;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.jpeg,
+  Vcl.StdCtrls;
+
+type
+  TSplashScn = class(TForm)
+    Image1: TImage;
+    Timer1: TTimer;
+    StaticText1: TStaticText;
+    StaticText2: TStaticText;
+    StaticText3: TStaticText;
+    procedure FormShow(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    Completed: Boolean;
+  end;
+
+var
+  SplashScn: TSplashScn;
+
+implementation
+
+{$R *.dfm}
+
+procedure TSplashScn.FormShow(Sender: TObject);
+begin
+   Timer1.Enabled := True;
+   Completed := False
+end;
+
+procedure TSplashScn.Timer1Timer(Sender: TObject);
+begin
+  Timer1.Enabled := False;
+  Completed := True;
+end;
+
+end.
