ebabc45f6b9be926be8fc19d423deb10b770c249
TexasFlyBoy <n2o4nto@gmail.com>
2018-10-03 12:27:32 -0500

    Started to add a Splash Screen. Created form with image and added code for form.
    Used the webpage: http://www.uweraabe.de/Blog/2016/01/22/a-splash-form-in-firemonkey/
    This has the Main Form and other Forms not be auto-created. This may get messy.
    
     On branch master
     Changes to be committed:
       modified:   SSCA_ACDR.DPR
       modified:   SSCA_ACDR.dproj
       modified:   SSCA_ACDR.identcache
       new file:   Splash.dfm
       new file:   Splash.pas

---
+++ Splash.pas
@@ -0,0 +1,67 @@
+unit Splash;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Imaging.jpeg,
+  Vcl.ExtCtrls;
+
+type
+  TFormSplash = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    StartupTimer: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure FormPaint(Sender: TObject);
+    procedure StartupTimerTimer(Sender: TObject);
+  private
+    { Private declarations }
+    FInitialized: Boolean;
+    procedure LoadMainForm;
+  public
+    { Public declarations }
+  end;
+
+var
+  FormSplash: TFormSplash;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFormSplash.FormCreate(Sender: TObject);
+begin
+  StartupTimer.Enabled := False;
+  StartupTimer.Interval := 500;
+end;
+
+procedure TFormSplash.FormPaint(Sender: TObject);
+begin
+   StartupTimer.Enabled := not FInitialized;
+end;
+
+procedure TFormSplash.LoadMainForm;
+var
+  form, form2: TForm;
+begin
+  form := TMainForm.Create(Application);
+  form.Show;
+  Application.MainForm := form;
+  Application.CreateForm(TAboutBox, AboutBox);
+
+  Close;
+end;
+
+procedure TFormSplash.StartupTimerTimer(Sender: TObject);
+begin
+  StartupTimer.Enabled := False
+  if not FInitialized then begin
+    FInitialized := True;
+    LoadMainForm;
+  end
+end;
+
+end.
